b87890e7be494ebfc5229abf9b0073052fd26939
guillaume <bague.guillaume@live.fr>
2018-10-25 20:30:46 +0200

    Add Slider
    add ViewPageAdapter class + activity_slide.xml

--- app/src/main/java/com/bague/guillaume/moodtracker/MainActivity.java
+++ app/src/main/java/com/bague/guillaume/moodtracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bague.guillaume.moodtracker;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -12,9 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mHistoryBtn = findViewById(R.id.mainActivity_History_Btn);
-        Button mCommentBtn = findViewById(R.id.mainActivity_Comment_Btn);
-        ImageView mImage = findViewById(R.id.mainActivity_Smiley_Image);
-        
+         Button mHistoryBtn;
+         Button mCommentBtn;
+         ViewPager mViewPager;
+         ViewPagerAdapter mViewPagerAdapter;
+
+        mHistoryBtn = findViewById(R.id.mainActivity_History_Btn);
+        mCommentBtn = findViewById(R.id.mainActivity_Comment_Btn);
+        mViewPager = findViewById(R.id.viewPager);
+        mViewPagerAdapter = new ViewPagerAdapter(this);
+        mViewPager.setAdapter(mViewPagerAdapter);
+
+
     }
 }
